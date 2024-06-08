aa11931e99f7a8866b0a38a7c388572036e23218
Humberto <hdkaratechop@gmail.com>
2024-06-08 12:39:07 -0700

    Integrate mongodb atlas search

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   root to: 'ideas#index'
   resources :ideas
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :searches, only: [:index]
+  get '/search_results', to: 'searches#display_results', as: 'search_results'
 end
